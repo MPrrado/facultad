cb09b595db9dae060cafb813057328c46f84a896
Prado Matias Santiago <matias14prado@gmail.com>
2025-03-28 14:22:31 -0300

    -

--- PARADIGMA/tps/tp2/ejercicio1.hs
+++
@@ -1,8 +0,0 @@
-calcularCuadrado x
- | x <= 0 = 0
- | x == 1 = 1
- | otherwise = 2*x + calcularCuadrado (x - 1) - 1
-
-{-
-lo que hace la funcion es elevar al cuadrado a la x, basicamente calcula el cuadrado de un numero
--}
--- PARADIGMA/tps/tp4/PruebaPC/pruebaHaskell.hs
+++
@@ -1,6 +0,0 @@
--- Prueba HASKELL
-
-misterio x 
-   |  x <= 0   =  0
-   |  x == 1   =  1
-   | otherwise =  2*x + misterio (x - 1) - 1
